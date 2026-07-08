1fac1e65ab763073f771e90021d26c160975fe35
samruddhiarabhavi <samruddhiarabhavi@gmail.com>
2026-07-08 22:25:27 +0530

    frontend setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App(){
+  return(
+    <div>
+      <h1> Smart Agri tech platform</h1>
+      <p> connecting rural workers to fair rural job providers</p>
+    </div>
+  )
+}
+export default App;
